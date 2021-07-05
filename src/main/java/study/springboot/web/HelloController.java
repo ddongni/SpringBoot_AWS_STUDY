29921a82a9d90f116eb967caf39e0a6af425cb73
ddongni <de0309@naver.com>
2021-07-05 20:30:25 +0900

    HelloController 코드 롬복 전환

--- src/main/java/study/springboot/web/HelloController.java
+++ src/main/java/study/springboot/web/HelloController.java
@@ -1,12 +1,18 @@
 package study.springboot.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import study.springboot.web.dto.HelloResponseDto;
 
 @RestController
 public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam String name, @RequestParam int amount){
+        return new HelloResponseDto(name, amount);
     }
 }
--- src/test/java/study/springboot/web/HelloControllerTest.java
+++ src/test/java/study/springboot/web/HelloControllerTest.java
@@ -6,9 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /* 테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행
 여기서는 SpringExtension 실행자 사용
@@ -37,5 +37,24 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void returnHelloDto() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        /* param
+            API 테스트 할 때 사용될 요청 파라미터 설정
+            단, 값은 String만 허용됨, 따라서 숫자/날짜 등의 데이터도 등록할 때는 문자열로 변경해야만 가능
+           jsonPath
+            JSON 응답값을 필드별로 검증할 수 있는 메소드, $를 기준으로 필드명을 명시
+         */
+        mvc.perform(get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
