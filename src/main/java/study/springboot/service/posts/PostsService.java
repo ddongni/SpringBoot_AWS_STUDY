34a6270573947ae15dfc86045bbea830efc8afb3
ddongni <de0309@naver.com>
2021-07-06 17:10:42 +0900

    등록/수정/조회 API 만들기

---
+++ src/main/java/study/springboot/service/posts/PostsService.java
@@ -0,0 +1,35 @@
+package study.springboot.service.posts;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import study.springboot.domain.posts.Posts;
+import study.springboot.domain.posts.PostsRepository;
+import study.springboot.web.dto.PostsResponseDto;
+import study.springboot.web.dto.PostsSaveRequestDto;
+import study.springboot.web.dto.PostsUpdateRequestDto;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id = "+id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id = "+id));
+        return new PostsResponseDto(entity);
+    }
+}
