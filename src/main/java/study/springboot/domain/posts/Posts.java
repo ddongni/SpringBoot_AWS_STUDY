f8c3e48daf04989b43644dd1b32d51270e233515
ddongni <de0309@naver.com>
2021-07-06 15:00:31 +0900

    Spring Data JPA 테스트 코드 작성 연습

---
+++ src/main/java/study/springboot/domain/posts/Posts.java
@@ -0,0 +1,54 @@
+package study.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/*
+ @Entity는 JPA 어노테이션, @Getter와 @NoArgsConstructor는 롬복의 어노테이션
+ @NoArgsConstructor
+    파라미터가 없는 기본 생성자 생성, public Posts(){}와 같은 효과
+ @Entity
+    테이블과 링크될 클래스임을 나타냄
+    ex) SalesManager.java -> sales_manager table 매칭
+*/
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    /*
+     @Id
+        해당 테이블의 PK 필드
+     @GeneratedValue
+        PK의 생성 규칙, 스프링 부트 2.0에서는 GenerationType.IDENTITY 옵션을 추가해야만 auto_increment가 됨
+        Long 타입은 MySQL 기준으로 bigint 타입이 됨
+     @Column
+        테이블의 칼럼을 나타내며 굳이 선언하지 않더라도 해당 클래스의 필드는
+        모두 칼럼, 사용하는 이유는 기본값 외에 추가로 변경이 필요한 옵션이 있으면 사용
+    */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    /*
+     @Builder
+        해당 클래스의 빌더 패턴 클래스를 생성, 생성자 상단에 선언 시 생성자에 포함된 필드만 빌더에 포함
+     */
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
--- src/test/java/study/springboot/web/HelloControllerTest.java
+++ src/test/java/study/springboot/web/HelloControllerTest.java
@@ -10,9 +10,15 @@ import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-/* 테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행
-여기서는 SpringExtension 실행자 사용
-즉, 스프링 부트 테스트와 JUnit 사이에 연결자 역할 */
+/*
+@ExtendWith(SpringExtension.class)
+    테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행
+    여기서는 SpringExtension 실행자 사용
+    즉, 스프링 부트 테스트와 JUnit 사이에 연결자 역할
+@WebMvcTest
+    여러 스프링 테스트 어노테이션 중, Web(Spring MVC)에 집중할 수 있는 어노테이션
+    @Controller, @ControllerAdvice 등을 사용할 수 있음
+ */
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class)
 public class HelloControllerTest {
