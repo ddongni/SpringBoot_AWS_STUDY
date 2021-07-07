407827b1895d7143f5b360cac2870fbc17e8cb32
ddongni <de0309@naver.com>
2021-07-07 15:15:03 +0900

    게시글 삭제 기능 추가

--- src/main/java/study/springboot/service/posts/PostsService.java
+++ src/main/java/study/springboot/service/posts/PostsService.java
@@ -49,4 +49,15 @@ public class PostsService {
          */
         return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
+
+    @Transactional
+    public void delete(Long id){
+        /*
+        JpaRepository에서 이미 delete메소드를 지원하고 있음.
+        엔티티를 파라미터로 삭제할 수도 있고, deleteById 메소드를 이용하면 id로 삭제 할 수도 잇음.
+        존재하는 Posts인지 확인을 위해 엔티티 조회 후 그대로 삭제.
+         */
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id = "+id));
+        postsRepository.delete(posts);
+    }
 }
