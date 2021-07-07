bb93e7b2ea678c89471e12c43891cfb95ab57837
ddongni <de0309@naver.com>
2021-07-07 13:09:20 +0900

    게시글 등록, 조회 화면 만들기

--- src/main/java/study/springboot/service/posts/PostsService.java
+++ src/main/java/study/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package study.springboot.service.posts;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import study.springboot.domain.posts.Posts;
 import study.springboot.domain.posts.PostsRepository;
+import study.springboot.web.dto.PostsListResponseDto;
 import study.springboot.web.dto.PostsResponseDto;
 import study.springboot.web.dto.PostsSaveRequestDto;
 import study.springboot.web.dto.PostsUpdateRequestDto;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,5 +34,19 @@ public class PostsService {
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id = "+id));
         return new PostsResponseDto(entity);
+    }
+
+    /*
+    readOnly = true를 주면 트랜잭션 범위는 유지하되, 조회 기능만 남겨두어 조회 속도 개선됨
+    등록, 수정, 삭제 기능이 전혀 없는 서비스 메소드에서 사용하는 것 추천
+    */
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        /*
+         .map(PostsListResponseDto::new)
+            실제로 .map(posts->new PostsListResponseDto(posts))와 같음.
+            postRepository 결과로 넘어온 Posts의 Stream을 map을 통해 PostListResponseDto 변환 -> List로 반환하는 메소드
+         */
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 }
--- src/main/java/study/springboot/web/IndexController.java
+++ src/main/java/study/springboot/web/IndexController.java
@@ -1,7 +1,10 @@
 package study.springboot.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import study.springboot.service.posts.PostsService;
 
 /*
  머스테치 스타터 덕분에 컨트롤러에서 문자열을 반환할 때
@@ -9,10 +12,25 @@ import org.springframework.web.bind.annotation.GetMapping;
  앞의 경로는 src/main/resources/templates로,
  뒤의 파일 확장자는 .mustache가 붙음
  */
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final PostsService postsService;
+
+    /*
+     Model
+        서버 템플릿 엔진에서 사용할 수 있는 객체 저장
+        여기서는 postsService.findAllDesc()로 가져온 결과를 posts로 index.musatche에 전달
+     */
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts",postsService.findAllDesc());
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
     }
 }
