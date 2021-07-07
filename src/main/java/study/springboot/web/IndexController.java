91b79076a83100ea70d3a0942380be27e9bf7f95
ddongni <de0309@naver.com>
2021-07-07 15:05:05 +0900

    게시글 수정 화면 만들기

--- src/main/java/study/springboot/web/IndexController.java
+++ src/main/java/study/springboot/web/IndexController.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import study.springboot.service.posts.PostsService;
+import study.springboot.web.dto.PostsResponseDto;
 
 /*
  머스테치 스타터 덕분에 컨트롤러에서 문자열을 반환할 때
@@ -32,5 +34,12 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post",dto);
+        return "posts-update";
     }
 }
