fb1deb388dc9e5a7b27ae183c6bed3ddd905886f
ddongni <de0309@naver.com>
2021-07-26 15:47:56 +0900

    네이버 로그인 연동

--- src/main/java/study/springboot/web/IndexController.java
+++ src/main/java/study/springboot/web/IndexController.java
@@ -34,7 +34,7 @@ public class IndexController {
     public String index(Model model , @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
         if(user!=null){
-            model.addAttribute("userName", user.getName());
+            model.addAttribute("user", user.getName());
         }
         return "index";
     }
